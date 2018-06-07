40280229da78bdface9e13fb4a78b47d72f51e1e
Siarhei Blashuk <siarhei.blashuk@godel.first-utility.com>
2018-06-08 00:14:33 +0300

    update

--- src/util/ModelsUtil.py
+++ src/util/ModelsUtil.py
@@ -216,3 +216,30 @@ def VGG_11(input_shape, num_classes):
     model.add(Dense(num_classes, activation='softmax'))
 
     return model
+
+
+def myVGG_11(input_shape, num_classes):
+    model = Sequential()
+
+    model.add(ZeroPadding2D((1,1),input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(2048, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2048, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    return model
